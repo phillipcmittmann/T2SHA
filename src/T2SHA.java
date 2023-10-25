6d42bb61f313afee19d3d84b307bf7e227326a8d
Phillip Correa Mittmann <phillip.mittmann@gmail.com>
2023-10-25 20:28:06 -0300

    Comentarios

--- src/T2SHA.java
+++ src/T2SHA.java
@@ -8,8 +8,9 @@ import java.security.NoSuchAlgorithmException;
 
 public class T2SHA {
 	public static void main(String args[]) throws IOException, NoSuchAlgorithmException {
-		FileInputStream video = new FileInputStream("FuncoesResumo - SHA1.mp4"); 
-        
+//		FileInputStream video = new FileInputStream("FuncoesResumo - SHA1.mp4"); 
+		FileInputStream video = new FileInputStream("FuncoesResumo - Hash Functions.mp4"); 
+		
 		int tamBloco = 1024;
         
         List<String> hexList = new ArrayList<>();
@@ -36,20 +37,27 @@ public class T2SHA {
         
         video.close();
 
+        // Instancia a classe de hashing
         MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
 
+        // Define os valores hexadecimais a serem resumidos
         sha256.update(hexList.get(hexList.size() - 1).getBytes());
         
+        // Armazena o valor do ultimo bloco em um array de bytes
         byte[] digest = sha256.digest();
         
+        // Armazena esse array de bytes em uma string
         String sha256Ult = String.format("%064x", new java.math.BigInteger(1, digest));
      	
         for (int i = hexList.size() - 2; i > 0; i--) {
+        	// Define o valor do bloco anterior + o bloco atual a ser resumido
         	sha256.update((hexList.get(i) + digest).getBytes());
         	
+        	// Armazena o hash num array de bytes
         	digest = sha256.digest();
         }
         
+        // Transforma o hash do primeiro bloco em uma string hexadecimal
         System.out.println(String.format("%064x", new java.math.BigInteger(1, digest)));
         System.out.println(sha256Ult);
 	}
